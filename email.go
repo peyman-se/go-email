93b7578c54bd1020beecd181fd1609762a33412e
Coder <hello@Hellos-MBP.phub.net.cable.rogers.com>
2021-05-02 00:35:55 -0400

    fix

--- email.go
+++ email.go
@@ -15,7 +15,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
-	"github.com/joho/godotenv"
 	gomail "gopkg.in/gomail.v2"
 )
 
